eae4536036f58f5344699f98f9387bf048de01dd
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-14 23:38:05 +0100

    add seasonsgreetings from ayo function

---
+++ script.js
@@ -0,0 +1,19 @@
+//seasonsGreetings from ayo function
+function ayo(seasonsGreetings){
+    alert(seasonsGreetings)
+
+}
+
+ayo("happy Easter");
+ayo("the king shall reign forever");
+ayo("if the lord extend is time, it will tarry");
+
+// subtraction function of 2 numbers 
+function sub(number1, number2,number3){
+    return number1 - number2
+}
+
+alert(sub(50,10));
+console.log(sub(100,50));
+document.write(sub(60,10));
+
