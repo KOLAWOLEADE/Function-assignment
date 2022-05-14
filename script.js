fb5a5dc9fdaf13b837d79745032df77929fefd04
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-15 00:25:36 +0100

    add subtraction function of 2 numbers

--- script.js
+++ script.js
@@ -8,12 +8,35 @@ ayo("happy Easter");
 ayo("the king shall reign forever");
 ayo("if the lord extend is time, it will tarry");
 
-// subtraction function of 2 numbers 
-function sub(number1, number2,number3){
-    return number1 - number2
+//  // subtraction function of 2 numbers 
+//  function sub(number1, number2){
+//      return number1 - number2
+//  }
+
+//   alert(sub(50,10));
+//  console.log(sub(50,10));
+// document.write(sub(50,10));
+
+
+// multiplication function
+function multiply(number1, number2){
+    return number1 * number2
 }
 
-alert(sub(50,10));
-console.log(sub(100,50));
-document.write(sub(60,10));
+alert (multiply(4, 8));
+console.log(multiply(4,8));
+document.write(multiply(4,8));
+
+
+// threeTimes function
+function threeTimes( number1, number2, number3){
+   return number1 * number2 * number3
+ }
+
+alert(multiply(1,9,10));
+console.log(multiply(1,9,10));
+document.write(multiply(1,9,10));
+
+
+// divi
 
