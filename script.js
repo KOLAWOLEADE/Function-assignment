4c0ed32d2cc23ea3f7d8a3f4cd586696f7e05e68
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-15 14:29:14 +0100

    add greetings function

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-//seasonsGreetings from ayo function
-function ayo(seasonsGreetings){
-    alert(seasonsGreetings)
+//  //seasonsGreetings from ayo function
+//  function ayo(seasonsGreetings){
+//     alert(seasonsGreetings)
 
-}
+//  }
 
-ayo("happy Easter");
-ayo("the king shall reign forever");
-ayo("if the lord extend is time, it will tarry");
+// ayo("happy Easter");
+// ayo("the king shall reign forever");
+// ayo("if the lord extend is time, it will tarry");
 
-//  // subtraction function of 2 numbers 
-//  function sub(number1, number2){
+// // subtraction function of 2 numbers 
+
+//   function sub(number1, number2){
 //      return number1 - number2
 //  }
 
 //   alert(sub(50,10));
-//  console.log(sub(50,10));
-// document.write(sub(50,10));
+//   console.log(sub(50,10));
+//   document.write(sub(50,10));
 
 
-// multiplication function
-function multiply(number1, number2){
-    return number1 * number2
-}
+// // multiplication function
 
-alert (multiply(4, 8));
-console.log(multiply(4,8));
-document.write(multiply(4,8));
+// function multiply(number1, number2){
+//     return number1 * number2
+// }
+
+// alert (multiply(4, 8));
+// console.log(multiply(4,8));
+// document.write(multiply(4,8));
 
 
-// threeTimes function
-function threeTimes( number1, number2, number3){
-   return number1 * number2 * number3
- }
+// // threeTimes function
 
-alert(multiply(1,9,10));
-console.log(multiply(1,9,10));
-document.write(multiply(1,9,10));
+// function threeTimes( number1, number2, number3){
+//    return number1 * number2 * number3
+//  }
+
+// alert(multiply(1,9,10));
+// console.log(multiply(1,9,10));
+// document.write(multiply(1,9,10));
 
 
 // // division function
@@ -46,14 +49,48 @@ document.write(multiply(1,9,10));
 
 
 
-// reminder function
+// // remainder function
+
+//
 function remainder(number1, number2){
-    return number1 % number2
+     return number1 % number2
+ }
+
+ alert(remainder(14, 3));
+ console.log(remainder(32,5));
+ document.write(remainder(8,3));
+
+
+// // infor function
+let name = prompt("what is your name?");
+let age  = prompt("How old are you ? ");
+let food = prompt("what's your favorite food");
+
+function greetings(name, age, food){
+return " what is your name? " +" "+ name + " how old are you ? " +" "+ age +  " what is your favorite food ? " + food;
+
 }
 
-alert(remainder(14, 3));
-console.log(remainder(32,5));
-document.write(remainder(8,3))
+alert(greetings(name, age, food));
+document.write(greetings(name, age, food))
+
+
+ 
+
+//  prompt("how old are you?");
+//  prompt("tell us your fullname");
+//  prompt("what your favorite pet?");
+
+
+// correction
+
+// let name = prompt ("what is your name?");
+
+// function greetings(name) {
+//   return "hello" +" " +name +"trust you are doing well ?"
+// }
+
+// document.write(greetings(name,))
 
 
 
